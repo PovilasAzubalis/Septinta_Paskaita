1b3a2948cc183906181506fe5dffa4d304c07442
azubalispovilas <azubalispovilas@gmail.com>
2024-05-31 11:24:31 +0300

    Add project files.

---
+++ 1PirmaUzduotis_darbuotojuSaugojimasFaile/Program.cs
@@ -0,0 +1,4 @@
+﻿// Užduotis 1: Sukurkite programą, kuri valdo darbuotojų duomenis. Programa leis pridėti darbuotojus, rodyti visus darbuotojus ir išsaugoti bei nuskaityti darbuotojų duomenis iš failo.
+// Pridėti darbuotoją į sąrašą
+// Pašalinti darbuotoją iš sąrašo
+// Parodyti visus darbuotojus
